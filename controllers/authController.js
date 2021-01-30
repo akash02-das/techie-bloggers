343bf7fd1c83a5af506aa2c444f8e4658d2c92f3
akash das <aku.das02@gmail.com>
2021-01-30 22:40:27 +0600

    hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.signupGetController = (request, response, next) => {
   response.render("pages/auth/signup", { title: "Create a new account" });
@@ -7,13 +8,15 @@ exports.signupGetController = (request, response, next) => {
 exports.signupPostController = async (request, response, next) => {
   let { username, email, password } = request.body;
 
-  let user = new User({
-    username,
-    email,
-    password,
-  });
-
   try {
+    let hashedPassword = await bcrypt.hash(password, 11);
+
+    let user = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
     let createdUser = await user.save();
     console.log("User created successfully", createdUser);
     response.render("pages/auth/signup", { title: "Create a new account" });
