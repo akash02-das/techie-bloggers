4bb4362cfbc6b534b8cd12a619b5204b58466f69
akash das <aku.das02@gmail.com>
2021-02-09 22:56:09 +0600

    upload multer middleware

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -10,3 +10,23 @@ const storage = multer.diskStorage({
     callback(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
   },
 });
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: (request, file, callback) => {
+    const types = /jpeg|jpg|png|gif/;
+    const extName = types.test(path.extname(file.originalname).toLowerCase());
+    const mimeType = types.test(file.mimetype);
+
+    if (extName && mimeType) {
+      callback(null, true);
+    } else {
+      callback(new Error("Only support images file"));
+    }
+  },
+});
+
+module.exports = upload;
