e08f82c27befaf96ec855f5527136800349ba90f
akash das <aku.das02@gmail.com>
2021-02-16 20:33:25 +0600

    tiny MCE backend

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,18 +1,26 @@
 const router = require("express").Router();
-const { isUnAuthenticated } = require("../middleware/authMiddleware");
+const { isAuthenticated } = require("../middleware/authMiddleware");
 const upload = require("../middleware/uploadMiddleware");
 const {
   uploadProfilePics,
   removeProfilePics,
+  postImageUploadController,
 } = require("../controllers/uploadController");
 
 router.post(
   "/profilePics",
-  isUnAuthenticated,
+  isAuthenticated,
   upload.single("profilePics"),
   uploadProfilePics
 );
 
-router.delete("/profilePics", isUnAuthenticated, removeProfilePics);
+router.delete("/profilePics", isAuthenticated, removeProfilePics);
+
+router.post(
+  "/postimage",
+  isAuthenticated,
+  upload.single("post-image"),
+  postImageUploadController
+);
 
 module.exports = router;
