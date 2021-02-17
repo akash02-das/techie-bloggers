429edd7cca896a83ac736f67a73061c790e26978
akash das <aku.das02@gmail.com>
2021-02-18 01:39:41 +0600

    created dislike controller api

--- api/controllers/likeDislikeController.js
+++ api/controllers/likeDislikeController.js
@@ -49,3 +49,53 @@ exports.likesGetController = async (req, res, next) => {
     });
   }
 };
+
+exports.dislikesGetController = async (req, res, next) => {
+  let { postId } = req.params;
+  let userId = req.user._id;
+  let disliked = null;
+
+  if (!req.user) {
+    return res.status(403).json({
+      error: "You are not an authenticated user",
+    });
+  }
+
+  try {
+    let post = await Post.findById(postId);
+
+    if (post.likes.includes(userId)) {
+      await Post.findOneAndUpdate(
+        { _id: postId },
+        { $pull: { likes: userId } }
+      );
+    }
+
+    if (post.dislikes.includes(userId)) {
+      await Post.findOneAndUpdate(
+        { _id: postId },
+        { $pull: { dislikes: userId } }
+      );
+      disliked = false;
+    } else {
+      await Post.findOneAndUpdate(
+        { _id: postId },
+        { $push: { dislikes: userId } }
+      );
+      disliked = true;
+    }
+
+    let updatedPost = await Post.findById(postId);
+
+    res.status(200).json({
+      disliked,
+      totalLikes: updatedPost.likes.length,
+      totalDislikes: updatedPost.dislikes.length,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Server Error Occurred",
+    });
+  }
+};
--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -1,19 +1,24 @@
 const router = require("express").Router();
 const { isAuthenticated } = require("../../middleware/authMiddleware");
+
 const {
   commentPostController,
   replyCommentPostController,
 } = require("../controllers/commentController");
 
-router.post("/comments/:postId", isAuthenticated, commentPostController);
+const {
+  likesGetController,
+  dislikesGetController,
+} = require("../controllers/likeDislikeController");
 
+router.post("/comments/:postId", isAuthenticated, commentPostController);
 router.post(
   "/comments/replies/:commentId",
   isAuthenticated,
   replyCommentPostController
 );
 
-router.get("/likes/:postId", isAuthenticated, (req, res) => {});
-router.get("/dislikes/:postId", isAuthenticated, (req, res) => {});
+router.get("/likes/:postId", isAuthenticated, likesGetController);
+router.get("/dislikes/:postId", isAuthenticated, dislikesGetController);
 
 module.exports = router;
