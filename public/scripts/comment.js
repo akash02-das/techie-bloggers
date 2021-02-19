506001725d22bb170a5a6582e1f5006f0a151c56
akash das <aku.das02@gmail.com>
2021-02-19 22:16:36 +0600

    reply comment from frontend

--- public/scripts/comment.js
+++ public/scripts/comment.js
@@ -29,6 +29,41 @@ window.onload = function () {
       }
     }
   });
+
+  commentHolder.addEventListener("keypress", function (e) {
+    if (commentHolder.hasChildNodes(e.target)) {
+      if (e.key === "Enter") {
+        let commentId = e.target.dataset.comment;
+        let value = e.target.value;
+
+        if (value) {
+          let data = {
+            body: value,
+          };
+
+          let req = generateRequest(
+            `/api/comments/replies/${commentId}`,
+            "POST",
+            data
+          );
+          fetch(req)
+            .then((res) => res.json())
+            .then((data) => {
+              let replyElement = createReply(data);
+              let parent = e.target.parentElement;
+              parent.previousElementSibling.appendChild(replyElement);
+              e.target.value = "";
+            })
+            .catch((e) => {
+              console.log(e.message);
+              alert(e.message);
+            });
+        } else {
+          alert("Please enter a valid reply");
+        }
+      }
+    }
+  });
 };
 
 function createComment(comment) {
@@ -47,6 +82,23 @@ function createComment(comment) {
 
   let div = document.createElement("div");
   div.className = "media border";
+  div.innerHTML = innerHTML;
+
+  return div;
+}
+
+function createReply(reply) {
+  let innerHTML = `
+        <img style="width:30px;"
+            src="${reply.profilePics}" 
+            class="align-self-start mx-5 rounded-circle">
+        <div class="media-body mx-5">
+            <p>${reply.body}</p>
+        </div>
+  `;
+
+  let div = document.createElement("div");
+  div.className = "media mt-3";
   div.innerHTML = innerHTML;
 
   return div;
