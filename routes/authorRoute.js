a6071f265b6d69cd3be313167f70855cfaa12615
akash das <aku.das02@gmail.com>
2021-02-20 01:57:38 +0600

    author page template

---
+++ routes/authorRoute.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const {
+  authorProfileGetController,
+} = require("../controllers/authorController");
+
+router.get("/:userId", authorProfileGetController);
+
+module.exports = router;
