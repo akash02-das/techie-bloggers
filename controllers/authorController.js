ccf25b8a5268d7d884664afab50e93ebae485496
akash das <aku.das02@gmail.com>
2021-02-20 03:55:56 +0600

    author controller and author page design tweak

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,8 +1,23 @@
+const User = require("../models/User");
 const Flash = require("../utils/Flash");
 
 exports.authorProfileGetController = async (req, res, next) => {
-  res.render("pages/explorer/author", {
-    title: "Author page",
-    flashMessage: Flash.getMessage(req),
-  });
+  let userId = req.params.userId;
+
+  try {
+    let author = await User.findById(userId).populate({
+      path: "profile",
+      populate: {
+        path: "posts",
+      },
+    });
+
+    res.render("pages/explorer/author", {
+      title: "Author page",
+      flashMessage: Flash.getMessage(req),
+      author,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
